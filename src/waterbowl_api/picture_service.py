535800a726522040049edeeb2d56e11231e9899d
Andrew LeVan <alsok@MacBook-Air.local>
2023-12-29 12:16:26 -0700

    isort

--- src/waterbowl_api/picture_service.py
+++ src/waterbowl_api/picture_service.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime
 from pathlib import Path
-from typing import Tuple, Union, Optional
+from typing import Optional, Tuple, Union
 
 import aiofiles
 import shortuuid
